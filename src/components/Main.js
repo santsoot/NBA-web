95d0bfdc6f4192752d82299f6c7abf33efa02f1a
kaiqi wang <santsoot@gmail.com>
2018-08-23 16:50:59 -0700

    profile-shotchart-skeleton

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,13 @@
 import React from 'react';
+import { Profile } from './Profile'
+import { ShotChart } from './ShotChart'
 
 export class Main extends React.Component {
     render() {
         return(
             <div className='main'>
-                <div>
-                    left
-                </div>
-                <div>
-                    right
-                </div>
+                <Profile/>
+                <ShotChart/>
             </div>
         )
     }
