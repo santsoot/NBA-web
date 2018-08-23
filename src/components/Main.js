bbdc77b809364acac164232751bd8cc8db579262
kaiqi wang <santsoot@gmail.com>
2018-08-23 16:34:59 -0700

    main

---
+++ src/components/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export class Main extends React.Component {
+    render() {
+        return(
+            <div className='main'>
+                <div>
+                    left
+                </div>
+                <div>
+                    right
+                </div>
+            </div>
+        )
+    }
+}
