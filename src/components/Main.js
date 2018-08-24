ee506bc58ab7ab9abf9c7cdd1ec2275936319e8a
kaiqi wang <santsoot@gmail.com>
2018-08-23 17:25:10 -0700

    shot-chart init

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import nba from 'nba';
 import { Profile } from './Profile'
 import { ShotChart } from './ShotChart'
 
+window.nba = nba;
+
 export class Main extends React.Component {
+    componentDidMount() {
+        console.log(nba.findPlayer("Brandon Ingram"));
+    }
+
     render() {
         return(
             <div className='main'>
                 <Profile/>
-                <ShotChart/>
+                <ShotChart playerId={1627742}/>
             </div>
         )
     }
