c1747de19f901452fec8606f01a2ca4523af21ae
kaiqi wang <santsoot@gmail.com>
2018-08-24 02:35:00 -0700

    shot chart props + slider init

---
+++ src/components/DataViewContainer.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { ShotChart } from './ShotChart';
+import { Slider, InputNumber, Row, Col } from 'antd';
+
+export class DataViewContainer extends React.Component {
+    state = {
+        minCount: 1,
+    }
+
+    onChange = (value) => {
+        this.setState({
+            minCount: value,
+        });
+    }
+
+    render() {
+        const { minCount } = this.state;
+        return(
+            <div className="data-view">
+                <ShotChart
+                    playerId={this.props.playerId}
+                    minCount={minCount}
+                    displayToolTips={true}
+                    chartType={"hexbin"}
+                />
+                <Row>
+                    <Col offset={4} span={12}>
+                        <Slider min={2} max={20} onChange={this.onChange} value={minCount} />
+                    </Col>
+                    <Col span={4}>
+                        <InputNumber
+                            min={2}
+                            max={20}
+                            style={{ marginLeft: 16 }}
+                            value={minCount}
+                            onChange={this.onChange}
+                        />
+                    </Col>
+                </Row>
+            </div>
+        )
+    }
+}
