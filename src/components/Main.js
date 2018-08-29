6b2c01e9ad303756773e5e776715319bc53c82e5
kaiqi wang <santsoot@gmail.com>
2018-08-29 14:07:04 -0700

    default info

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import nba from 'nba';
 import { Profile } from "./Profile";
-import { DataViewContainer } from './DataViewContainer'
-import { SearchBar } from './SearchBar'
+import { DataViewContainer } from './DataViewContainer';
+import { SearchBar } from './SearchBar';
+import { DEFAULT_PLAYER_INFO } from './constants'
+
+window.nba = nba;
 
 export class Main extends React.Component {
     state = {
-        playerInfo: {
-            playerId: nba.findPlayer("Brandon Ingram").playerId,
-        },
+        playerInfo: DEFAULT_PLAYER_INFO
     }
 
     componentDidMount() {
-        this.loadPlayerInfo("Brandon Ingram");
+        this.loadPlayerInfo(this.state.playerInfo.playerName);
     }
 
     loadPlayerInfo = (playerName) => {
